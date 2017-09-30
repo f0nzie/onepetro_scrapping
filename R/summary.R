874b2a4a17d97f5b7fe5b8f1e344d8872b39cccd
Alfonso Reyes <alfonso.r.reyes@gmail.com>
2017-09-29 21:55:57 -0500

    added functions summary_by_dates, summary_by_publications and publication_result_left to extract since date and by parent publication.

--- R/summary.R
+++ R/summary.R
@@ -1,8 +1,16 @@
 library(rvest)
 
-publication_result <- function(result) {
+publication_result_right <- function(result) {
     pub_doc <- result %>%
         html_nodes(".facet-unit-right") %>%
+        html_nodes("option") %>%
+        html_text()
+    pub_doc
+}
+
+publication_result_left <- function(result) {
+    pub_doc <- result %>%
+        html_nodes(".facet-unit-left") %>%
         html_nodes("option") %>%
         html_text()
     pub_doc
@@ -23,6 +31,21 @@ summary_by_doctype <- function(x) {
 }
 
 
+summary_by_dates <- function(x) {
+    pub_vector <- get_dc_issued_year(x)
+    pub_values <- extract_num_papers(pub_vector)
+    pub_name   <- extract_publishers(pub_vector)
+    data.frame(pub_name, pub_values, stringsAsFactors = FALSE)
+}
+
+summary_by_publications <- function(x) {
+    pub_vector <- get_s2_parent_title(x)
+    pub_values <- extract_num_papers(pub_vector)
+    pub_name   <- extract_publishers(pub_vector)
+    data.frame(pub_name, pub_values, stringsAsFactors = FALSE)
+}
+
+
 get_dctype <- function(aList) {
     len_list <- length(aList)
     ix <- grep("All types", aList)
@@ -34,6 +57,18 @@ get_dc_publisher <- function(aList) {
     aList[2:(ix_stop-1)]
 }
 
+get_dc_issued_year <- function(aList) {
+    ix_stop <- grep("All publications", aList)
+    aList[2:(ix_stop-1)]
+}
+
+get_s2_parent_title <- function(aList) {
+    len_list <- length(aList)
+    ix <- grep("All publications", aList)
+    aList[(ix+1):len_list]
+}
+
+
 
 extract_num_papers <- function(x) {
     pattern <- "(?<=\\{).+(?=\\})"
@@ -43,7 +78,8 @@ extract_num_papers <- function(x) {
 
 
 extract_publishers <- function(x) {
-    pattern <- "(\\s[{\\d}].+)"
+    # pattern <- "(\\s[{\\d}].+)"
+    pattern <- "\\s{([0-9].*)}"
     gsub(pattern, "", x, perl = TRUE)
 }
 
